f1819b93b5c83bf70ae657f16502b6ad53c34a3e
rayking12 <rayodo667@gmail.com>
2021-01-22 11:45:33 +0100

    added edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,15 @@ class UsersController < ApplicationController
     def edit
       @user = User.find(params[:id])
     end
+    
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    end
 
     private
     

01f488baba3a9dcc15468e4029f0a54e9b9b0135
helciodev <helcioandre@live.com>
2021-01-22 09:50:04 +0100

    changes view for new user to use form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,18 @@ class UsersController < ApplicationController
         @users = User.all
     end
     def new
+        @user = User.new()
     end
+
     def create
-    end
+        @user = User.new(user_params)
+      
+        if @user.save
+          redirect_to new_user_path
+        else
+          render :new
+        end
+      end
 
     def edit
       @user = User.find(params[:id])

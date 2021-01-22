d8929393a73e26a03f02b1f1c761fdb9975dbee8
helciodev <helcioandre@live.com>
2021-01-22 13:28:44 +0100

    adds form edit method
    adds form helper to render on the edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,13 +21,13 @@ class UsersController < ApplicationController
     end
     
     def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-        redirect_to @user
-      else
-        render 'edit'
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+          redirect_to edit_user_path
+        else
+          render :edit
+        end
       end
-    end
 
     private
     
